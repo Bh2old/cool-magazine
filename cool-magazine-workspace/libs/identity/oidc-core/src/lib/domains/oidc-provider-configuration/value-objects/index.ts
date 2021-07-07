97b505ce6a550d79db2036a5ed452eb80d7b71ad
bh2old <bh2old@gmail.com>
2021-07-07 19:13:28 +0500

    refactor(oidc-core): add index.ts for value-object folder

---
+++ cool-magazine-workspace/libs/identity/oidc-core/src/lib/domains/oidc-provider-configuration/value-objects/index.ts
@@ -0,0 +1,6 @@
+export * from "./client-id";
+export * from "./client-metadata";
+export * from "./client-secret";
+export * from "./grant-type";
+export * from "./redirect-uri";
+export * from "./response-type";
