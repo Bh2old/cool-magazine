6553f5d1a9cf02d8893894c7d420306f16820f45
bh2old <bh2old@gmail.com>
2021-07-16 22:36:49 +0500

    refactor(oidc-core): improve tests for StaticClient
    renamed descriptions tests

--- cool-magazine-workspace/libs/identity/oidc-core/src/lib/domains/oidc-provider-configuration/entities/static-client/__tests__/static-client.spec.ts
+++ cool-magazine-workspace/libs/identity/oidc-core/src/lib/domains/oidc-provider-configuration/entities/static-client/__tests__/static-client.spec.ts
@@ -8,31 +8,33 @@ import {
 import { IClientRegistrationData } from '../models';
 import { StaticClient } from '../static-client.entity';
 
-const idValue = '123';
-const id: ClientId = ClientId.create(idValue);
-
-const redirectUriValue = 'http://qwerty.mn/';
-const redirectUri: RedirectUri = RedirectUri.create(redirectUriValue);
-
-const responseTypeValue = 'code';
-const responseType: ResponseType = ResponseType.create(responseTypeValue);
-
-const grantTypeValue = 'access_code';
-const grantType: GrantType = GrantType.create(grantTypeValue);
-
-const metadata: ClientMetadata = ClientMetadata.create({
-  redirectUris: [redirectUri],
-  responseTypes: [responseType],
-  grantTypes: [grantType],
-});
-
-const clientRegistrationData: IClientRegistrationData = {
-  id,
-  metadata,
-};
-
 describe('StaticClient', () => {
-  describe('create', () => {
+  let clientRegistrationData: IClientRegistrationData;
+  beforeEach(() => {
+    const idValue = '123';
+    const id: ClientId = ClientId.create(idValue);
+
+    const redirectUriValue = 'http://qwerty.mn/';
+    const redirectUri: RedirectUri = RedirectUri.create(redirectUriValue);
+
+    const responseTypeValue = 'code';
+    const responseType: ResponseType = ResponseType.create(responseTypeValue);
+
+    const grantTypeValue = 'access_code';
+    const grantType: GrantType = GrantType.create(grantTypeValue);
+
+    const metadata: ClientMetadata = ClientMetadata.create({
+      redirectUris: [redirectUri],
+      responseTypes: [responseType],
+      grantTypes: [grantType],
+    });
+
+    clientRegistrationData = {
+      id,
+      metadata,
+    };
+  });
+  describe('instance creation', () => {
     test('should return instance of StaticClient', () => {
       expect(StaticClient.create(clientRegistrationData)).toBeInstanceOf(
         StaticClient
@@ -40,10 +42,15 @@ describe('StaticClient', () => {
     });
   });
 
-  describe('clone', () => {
-    test('should return clone of StaticClient', () => {
+  describe('getting immutable clone of yourself ', () => {
+    test('should return cloned instance of StaticClient', () => {
+      // Arrange
       const staticClient = StaticClient.create(clientRegistrationData);
+
+      // Act
       const clonedStaticClient = staticClient.clone();
+
+      // Assert
       expect(staticClient === clonedStaticClient).toBeFalsy();
     });
   });
