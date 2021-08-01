13d597108da70e2b7913be5a7c438e99fc323d69
bh2old <bh2old@gmail.com>
2021-08-02 01:31:22 +0500

    feat(ddd-expc): add specification localhost reg-name host uri

--- cool-magazine-workspace/libs/shared/ddd-expc/src/lib/implementations/specifications/index.ts
+++ cool-magazine-workspace/libs/shared/ddd-expc/src/lib/implementations/specifications/index.ts
@@ -2,3 +2,4 @@ export * from './length-string-specification';
 export * from './absolute-url-specification';
 export * from './scheme-uri-component-specification';
 export * from './host-uri-authority-specification';
+export * from './localhost-reg-name-host-uri-authority-specification';
