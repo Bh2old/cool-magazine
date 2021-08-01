3ce86dc32ac8666626ec4d4cff10e2e78730fb89
bh2old <bh2old@gmail.com>
2021-08-02 01:32:13 +0500

    feat(ddd-expc): add specification localhost IPv4 host uri

--- cool-magazine-workspace/libs/shared/ddd-expc/src/lib/implementations/specifications/index.ts
+++ cool-magazine-workspace/libs/shared/ddd-expc/src/lib/implementations/specifications/index.ts
@@ -3,3 +3,4 @@ export * from './absolute-url-specification';
 export * from './scheme-uri-component-specification';
 export * from './host-uri-authority-specification';
 export * from './localhost-reg-name-host-uri-authority-specification';
+export * from './localhost-ipv4-host-uri-authority-specification';
---
+++ cool-magazine-workspace/libs/shared/ddd-expc/src/lib/implementations/specifications/localhost-ipv4-host-uri-authority-specification.ts
@@ -0,0 +1,16 @@
+import {
+  CompositeSpecification,
+  IHostUriAuthorityPart,
+} from '../../abstractions';
+import { HostUriAuthoritySpecification } from './host-uri-authority-specification';
+
+type LocalhostIPv4HostCandidate = IHostUriAuthorityPart | string;
+
+export class LocalhostIPv4HostUriAuthoritySpecification extends CompositeSpecification<LocalhostIPv4HostCandidate> {
+  private readonly _hostUriAuthoritySpecification =
+    new HostUriAuthoritySpecification('127.0.0.1', true);
+
+  isSatisfiedBy(candidate: LocalhostIPv4HostCandidate): boolean {
+    return this._hostUriAuthoritySpecification.isSatisfiedBy(candidate);
+  }
+}
