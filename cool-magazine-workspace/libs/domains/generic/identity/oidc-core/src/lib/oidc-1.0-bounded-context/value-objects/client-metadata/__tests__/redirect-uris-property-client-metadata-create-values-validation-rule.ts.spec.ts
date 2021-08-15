37f3fbec565d29208d3b69c22fffdb8e2cbb7e52
bh2old <bh2old@gmail.com>
2021-08-15 17:21:25 +0500

    feat(oidc-core): add redirect uris property validation rule
    For IClientMetadataCreateValues model

---
+++ cool-magazine-workspace/libs/domains/generic/identity/oidc-core/src/lib/oidc-1.0-bounded-context/value-objects/client-metadata/__tests__/redirect-uris-property-client-metadata-create-values-validation-rule.ts.spec.ts
@@ -0,0 +1,142 @@
+import { RedirectUrisPropertyClientMetadataCreateValuesValidationRule } from '../rules';
+
+describe('RedirectUrisPropertyClientMetadataCreateValuesValidationRule', () => {
+  describe('instance creation', () => {
+    test('should return instance', () => {
+      // Arrange
+      // Act
+      const instance =
+        new RedirectUrisPropertyClientMetadataCreateValuesValidationRule();
+
+      // Assert
+      expect(instance).toBeInstanceOf(
+        RedirectUrisPropertyClientMetadataCreateValuesValidationRule
+      );
+    });
+  });
+
+  describe('validation', () => {
+    test('should return true if all uris are valid', () => {
+      // Arrange
+      const uris = {
+        redirectUris: new Set<string>()
+          .add('http://localhost:1234/path')
+          .add('http://127.0.0.1:1234/path')
+          .add('https://domain.dom/path'),
+      };
+      const instance =
+        new RedirectUrisPropertyClientMetadataCreateValuesValidationRule();
+
+      // Act
+      const result = instance.validate(uris);
+
+      // Assert
+      expect(result).toBe(true);
+    });
+
+    test('should return false if at least one not localhost uri does not contain https scheme', () => {
+      // Arrange
+      const uris = {
+        redirectUris: new Set<string>()
+          .add('http://localhost:1234/path')
+          .add('https://domain.dom/path')
+          .add('http://domain.dom/path'),
+      };
+      const instance =
+        new RedirectUrisPropertyClientMetadataCreateValuesValidationRule();
+
+      // Act
+      const result = instance.validate(uris);
+
+      // Assert
+      expect(result).toBe(false);
+    });
+
+    test('should return false if at least one not localhost uri contains ipv4 host', () => {
+      // Arrange
+      const uris = {
+        redirectUris: new Set<string>()
+          .add('https://domain.dom/path')
+          .add('http://127.0.0.1:1234/path')
+          .add('https://123.5.6.7:1234/path'),
+      };
+      const instance =
+        new RedirectUrisPropertyClientMetadataCreateValuesValidationRule();
+
+      // Act
+      const result = instance.validate(uris);
+
+      // Assert
+      expect(result).toBe(false);
+    });
+
+    test('should return false if at least one not localhost uri contains ipv6 host', () => {
+      // Arrange
+      const uris = {
+        redirectUris: new Set<string>()
+          .add('https://domain.dom/path')
+          .add('http://127.0.0.1:1234/path')
+          .add('https://[1:2:3:4:5:6:7:8]/path'),
+      };
+      const instance =
+        new RedirectUrisPropertyClientMetadataCreateValuesValidationRule();
+
+      // Act
+      const result = instance.validate(uris);
+
+      // Assert
+      expect(result).toBe(false);
+    });
+
+    test('should return false if at least one uri is not absolute', () => {
+      // Arrange
+      const uris = {
+        redirectUris: new Set<string>()
+          .add('https://domain.dom/path')
+          .add('domain.com/path'),
+      };
+      const instance =
+        new RedirectUrisPropertyClientMetadataCreateValuesValidationRule();
+
+      // Act
+      const result = instance.validate(uris);
+
+      // Assert
+      expect(result).toBe(false);
+    });
+
+    test('should return false if at least one uri contains traversal path', () => {
+      // Arrange
+      const uris = {
+        redirectUris: new Set<string>()
+          .add('https://domain.dom/path')
+          .add('https://domain.com/../file.png'),
+      };
+      const instance =
+        new RedirectUrisPropertyClientMetadataCreateValuesValidationRule();
+
+      // Act
+      const result = instance.validate(uris);
+
+      // Assert
+      expect(result).toBe(false);
+    });
+
+    test('should return false if at least one uri contains authority user info', () => {
+      // Arrange
+      const uris = {
+        redirectUris: new Set<string>()
+          .add('https://domain.dom/path')
+          .add('https://log:pas@domain.com'),
+      };
+      const instance =
+        new RedirectUrisPropertyClientMetadataCreateValuesValidationRule();
+
+      // Act
+      const result = instance.validate(uris);
+
+      // Assert
+      expect(result).toBe(false);
+    });
+  });
+});
---
+++ cool-magazine-workspace/libs/domains/generic/identity/oidc-core/src/lib/oidc-1.0-bounded-context/value-objects/client-metadata/rules/validations/redirect-uris-property-client-metadata-create-values-validation-rule.ts
@@ -0,0 +1,30 @@
+import { ValidationRule } from '@bh2old/ddd-expc';
+import { IClientMetadataCreateValues } from '../../models';
+import { RedirectUrisPropertyClientMetadataCreateValuesSpecification } from '../../specifications';
+
+export class RedirectUrisPropertyClientMetadataCreateValuesValidationRule extends ValidationRule<IClientMetadataCreateValues> {
+  constructor() {
+    const rules = {
+      scheme:
+        'URIs must use the HTTPS scheme, not plain HTTP. Localhost URIs (including localhost IP address URIs) are exempt from this rule.',
+      host: 'Hosts cannot be raw IP addresses. Localhost IP addresses are exempted from this rule.',
+      userInfo: 'Redirect URIs cannot contain the userinfo subcomponent.',
+      path: 'Redirect URIs cannot contain a path traversal (also called directory backtracking), which is represented by an “/..” or “..” or their URL encoding.',
+    };
+
+    const message = Object.entries(rules)
+      .reduce((rulesText, rule, index) => {
+        const [ruleName, ruleDescription] = rule;
+        return (rulesText += `${index + 1}) ${ruleName}: ${ruleDescription} `);
+      }, '')
+      .trim();
+
+    const property = 'redirectUris';
+
+    super(
+      new RedirectUrisPropertyClientMetadataCreateValuesSpecification(),
+      message,
+      property
+    );
+  }
+}
