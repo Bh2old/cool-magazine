49ceb43d75b73821695f3bfa1495b14d5d4e6c3b
bh2old <bh2old@gmail.com>
2021-08-15 21:35:58 +0500

    feat(oidc-core): add response type variants property specification
    For IClientMetadataCreateValues model

---
+++ cool-magazine-workspace/libs/domains/generic/identity/oidc-core/src/lib/oidc-1.0-bounded-context/value-objects/client-metadata/__tests__/response-type-variants-property-client-metadata-create-values-specification.spec.ts
@@ -0,0 +1,70 @@
+import { ResponseTypeVariants } from '../../response-type';
+import { IClientMetadataCreateValues } from '../models';
+import { ResponseTypeVariantsPropertyClientMetadataCreateValuesSpecification } from '../specifications';
+
+describe('ResponseTypeVariantsPropertyClientMetadataCreateValuesSpecification', () => {
+  describe('instance creation', () => {
+    test('should return instance', () => {
+      // Arrange
+      // Act
+      const instance =
+        new ResponseTypeVariantsPropertyClientMetadataCreateValuesSpecification();
+
+      // Assert
+      expect(instance).toBeInstanceOf(
+        ResponseTypeVariantsPropertyClientMetadataCreateValuesSpecification
+      );
+    });
+  });
+
+  describe('requirements checking', () => {
+    test('should return true if variants are undefined', () => {
+      // Arrange
+      const variants: IClientMetadataCreateValues = {
+        redirectUris: new Set<string>(),
+      };
+      const instance =
+        new ResponseTypeVariantsPropertyClientMetadataCreateValuesSpecification();
+
+      // Act
+      const result = instance.isSatisfiedBy(variants);
+
+      // Assert
+      expect(result).toBe(true);
+    });
+
+    test('should return true if variants are specified', () => {
+      // Arrange
+      const variants: IClientMetadataCreateValues = {
+        redirectUris: new Set<string>(),
+        responseTypeVariants: new Set<ResponseTypeVariants>().add('code'),
+      };
+
+      const instance =
+        new ResponseTypeVariantsPropertyClientMetadataCreateValuesSpecification();
+
+      // Act
+      const result = instance.isSatisfiedBy(variants);
+
+      // Assert
+      expect(result).toBe(true);
+    });
+
+    test('should return false if variants are not specified', () => {
+      // Arrange
+      const variants: IClientMetadataCreateValues = {
+        redirectUris: new Set<string>(),
+        responseTypeVariants: new Set<ResponseTypeVariants>(),
+      };
+
+      const instance =
+        new ResponseTypeVariantsPropertyClientMetadataCreateValuesSpecification();
+
+      // Act
+      const result = instance.isSatisfiedBy(variants);
+
+      // Assert
+      expect(result).toBe(false);
+    });
+  });
+});
---
+++ cool-magazine-workspace/libs/domains/generic/identity/oidc-core/src/lib/oidc-1.0-bounded-context/value-objects/client-metadata/specifications/response-type-variants-property-client-metadata-create-values-specification.ts
@@ -0,0 +1,18 @@
+import { CompositeSpecification } from '@bh2old/ddd-expc';
+import { CreateManyVariantsResponseTypeSpecification } from '../../response-type';
+import { IClientMetadataCreateValues } from '../models';
+
+export class ResponseTypeVariantsPropertyClientMetadataCreateValuesSpecification extends CompositeSpecification<IClientMetadataCreateValues> {
+  private readonly _createManyVariantsResponseTypeSpecification =
+    new CreateManyVariantsResponseTypeSpecification();
+
+  isSatisfiedBy(candidate: IClientMetadataCreateValues): boolean {
+    if (candidate.responseTypeVariants === undefined) {
+      return true;
+    }
+
+    return this._createManyVariantsResponseTypeSpecification.isSatisfiedBy(
+      candidate.responseTypeVariants
+    );
+  }
+}
