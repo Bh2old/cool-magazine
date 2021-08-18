f7291866cac49bc0b7b8862a0208fb0f8c2735ff
bh2old <bh2old@gmail.com>
2021-08-19 01:37:20 +0500

    fix(ddd-expc): add exports for ValidationRulesChecker abstractions

--- cool-magazine-workspace/libs/shared/ddd-expc/src/lib/abstractions/classes/validations/index.ts
+++ cool-magazine-workspace/libs/shared/ddd-expc/src/lib/abstractions/classes/validations/index.ts
@@ -1,1 +1,4 @@
 export * from './validation-rule';
+export * from './validation-rules-checking-result-error';
+export * from './validation-rules-checking-result';
+export * from './validation-rules-checker';
