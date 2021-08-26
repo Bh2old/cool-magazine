3bb45b9fe5142e2991cfd59b7933d305d725febb
bh2old <bh2old@gmail.com>
2021-08-26 20:45:11 +0500

    build(@nrwl): migrate latest

--- cool-magazine-workspace/jest.config.js
+++ cool-magazine-workspace/jest.config.js
@@ -1,5 +1,8 @@
+const { getJestProjects } = require('@nrwl/jest');
+
 module.exports = {
   projects: [
+    ...getJestProjects(),
     '<rootDir>/apps/services/identity-service',
     '<rootDir>/libs/adapters/identity/node-oidc-provider',
     '<rootDir>/libs/domains/generic/identity/oidc-core',
