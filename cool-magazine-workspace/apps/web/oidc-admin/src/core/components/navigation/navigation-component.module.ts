0cad3057192e8a23596127474520782348a492c4
bh2old <bh2old@gmail.com>
2021-08-31 00:33:39 +0500

    feat(web-oidc-admin): add skatched navigation style and drafted using
    affected: shared-styles library

--- cool-magazine-workspace/apps/web/oidc-admin/src/core/components/navigation/navigation-component.module.ts
+++ cool-magazine-workspace/apps/web/oidc-admin/src/core/components/navigation/navigation-component.module.ts
@@ -1,8 +1,10 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { NavigationComponent } from './navigation.component';
 
 @NgModule({
   declarations: [NavigationComponent],
   exports: [NavigationComponent],
+  imports: [RouterModule],
 })
 export class NavigationComponentModule {}
--- cool-magazine-workspace/apps/web/oidc-admin/src/core/components/navigation/navigation.component.ts
+++ cool-magazine-workspace/apps/web/oidc-admin/src/core/components/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { ClientsAppRouteLink, HomeAppRouteLink } from '../../routing';
 
 @Component({
   selector: 'app-navigation',
@@ -6,4 +7,16 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./navigation.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavigationComponent {}
+export class NavigationComponent {
+  private readonly _homeAppRouteLink = new HomeAppRouteLink();
+  private readonly _clientsAppRouteLink = new ClientsAppRouteLink();
+
+  private readonly _homeLink = `/${this._homeAppRouteLink.getLink()}`;
+  get homeLink() {
+    return this._homeLink;
+  }
+  private readonly _clientsLink = `/${this._clientsAppRouteLink.getLink()}`;
+  get clientsLink() {
+    return this._clientsLink;
+  }
+}
